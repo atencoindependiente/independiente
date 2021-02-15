879e4f01c79639f08894b87f0769c1ccc5b6e677
JuanmanuelDLR <juanmanuel_delucio@yahoo.com>
2021-02-15 14:23:14 -0600

    Se agregan las nuevas imagenes

--- ra.web/js/index.js
+++ ra.web/js/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     //$("#ModalCenterInicioSesion").modal('show');
     //console.log("index.js");
+    $("#alert_valida_campos").hide();
 });
+
+function mostrarVideo() {
+    $(".mfp-iframe-holder").show();
+}
